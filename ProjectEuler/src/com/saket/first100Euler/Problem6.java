646533d869e5b8fe91b7bb53ecd75936c47539dc
Saket Patankar <saket.patankar@gmail.com>
2015-07-27 14:37:35 -0400

    Problem 6 solved.

--- ProjectEuler/src/com/saket/first100Euler/Problem6.java
+++ ProjectEuler/src/com/saket/first100Euler/Problem6.java
@@ -16,7 +16,9 @@ package com.saket.first100Euler;
 public class Problem6 {
 
 	public static void main(String[] args) {
-
+		//Start timer to calculate efficiency
+		long start = System.nanoTime();
+		
 		//Sum of squares
 		long sumOfSquares=0;
 		for(int i=1;i<=100;i++){
@@ -33,7 +35,35 @@ public class Problem6 {
 		System.out.println("The square of sums of the first 100 natural numbers is " + squareOfSums);
 		
 		System.out.println("Difference is : " + (squareOfSums - sumOfSquares));
+		
+		long stop = System.nanoTime();
+	    System.out.println("Time: " + (stop - start) / 1000 + " microseconds");
+	    
+	    //Found an optimized method for the same... 
+	    optimizedMethod();
+	}
+	
+	//Using arithmatic formulae for calculating the sum of n numbers and the square of sum of n numbers 
+	//We get the following solutions.
+	public static void optimizedMethod(){
 
+		long start = System.nanoTime();
+		long sum = 0;
+		long squared = 0;
+		long result = 0;
+		 
+		int N = 100;
+		 
+		sum = N * (N+1)/ 2;
+		squared = (N * (N + 1) * (2 * N + 1)) / 6;
+		
+		result = sum * sum - squared;
+		
+		System.out.println("Difference is for new method :" + result);
+		
+		long stop = System.nanoTime();
+	    System.out.println("New Time: " + (stop - start) / 1000 + " microseconds");
+		
 	}
 
 }
